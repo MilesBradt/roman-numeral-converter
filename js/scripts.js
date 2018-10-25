602876ce825364c08b05da88ff368a8a311cdb22
Devin Sweeting and Mike Picker <mike@>
2018-10-25 11:25:13 -0700

    add function for counting up to 8

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-function translate1(num) {
-  if (num === 1) {
-    return "I";
-  }
-}
+// function translate1(num) {
+//   if (num === 1) {
+//     return "I";
+//   }
+// }
 
 function countTo3(num) {
   var countUp= []
@@ -23,10 +23,25 @@ function countTo4(num) {
 function countTo5(num) {
   if (num <= 4) {
     return countTo4(num);
-  } else if (num === 5) {
+  } else if (num >= 5) {
     return "V";
   }
 }
+
+function countTo8(num) {
+  if (num <= 5) {
+    return countTo5(num);
+  } else if (num > 5 && num % 5 <= 3) {
+   return countTo5(num) + countTo3(num % 5);
+  }
+}
+
+  //  {
+  //     countUp.push("I")};
+  // } else if (num <= 5) {
+  //     return countTo5(num);
+  //   }
+  // }
 
 
 
@@ -39,7 +54,7 @@ function countTo5(num) {
 $(document).ready(function() {
   $("#form1").submit(function() {
     event.preventDefault();
-    var romanNumeral = countTo5(parseInt($("input#number").val()));
+    var romanNumeral = countTo8(parseInt($("input#number").val()));
     console.log(romanNumeral);
   })
 
